f9750d553391b25f30dfcfa16a66ec367c426185
Daniel Ramirez-Escudero <daniramirezescudero@Daniels-MacBook-Pro.local>
2017-10-04 12:54:52 +0200

    added stuff

--- function.js
+++ function.js
@@ -2,9 +2,27 @@
  * Created by daniramirezescudero on 04/05/2017.
  */
 
-
 (function(){
     'use strict';
+
+        var menuButton = document.querySelector('[data-btn-menu-mobile]');
+        var closeMenu = document.querySelector('[data-btn-close-menu]');
+        var header = document.querySelector('[data-header]');
+        var menu = document.querySelector('[data-menu]');
+
+        menuButton.addEventListener('click', function() {
+            /* do stuff here*/
+            menu.classList.toggle('fade-in');
+        }, false);
+
+        document.querySelector('[data-btn-close-menu]').addEventListener('click', function() {
+            /* do stuff here*/
+            menu.classList.remove('fade-in');
+        }, false);
+
+
+
+
 
     // var homeHeaderLink = document.querySelector('[data-more-button-intro]');
 
